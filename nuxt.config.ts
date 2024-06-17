35941827cc890a5a6fcdc38ac24fa39f8fe1239a
sumi <sumi>
2024-06-17 13:57:05 +0900

    ssr解除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
      modules: ["nuxt-microcms-module", "@nuxt/ui"],
      microCMS: {
        serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
